3a34e1684859158833ddd5390eb7ce80d175585f
Tony Fontanilla <tfont2020@outlook.com>
2022-08-14 06:26:32 -0700

    Style updates from public site.

--- src/pages/Decoder.svelte
+++ src/pages/Decoder.svelte
@@ -312,6 +312,7 @@
 <main>
 	<div class="fixed">
 		<center>
+			
 			<TriangularNumbers bind:triangularHighlight />
 			<FibonacciNumbers bind:fibonacciHighlight />
 			<nav>
@@ -465,6 +466,12 @@
 		text-align: center;
 		max-width: 240px;
 		margin: 0px;
+	}
+
+	select {
+		color: yellow;
+		background-color:blue;
+		box-shadow: 9px 7px 8px rgba(200, 14, 224, 0.863)
 	}
 
 	h1 {
