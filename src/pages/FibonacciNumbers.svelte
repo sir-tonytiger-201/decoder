378b0a70bdff1e2cace0bfec5a13b66aa077a031
Tony Fontanilla <tfont2020@outlook.com>
2022-08-14 06:57:46 -0700

    fixes

--- src/pages/FibonacciNumbers.svelte
+++ src/pages/FibonacciNumbers.svelte
@@ -3,53 +3,56 @@
   let fibonacciArray = [];
   export let fibonacciHighlight = {};
 
-
-  $fibonacciNumbers.forEach(m => {
+  $fibonacciNumbers.forEach((m) => {
     fibonacciArray.push(m);
     fibonacciHighlight[m] = false;
-  }); 
-  
-  
+  });
 
-
-  
-  
-    const toggleHighlight = num => {
-    fibonacciHighlight[num] = !fibonacciHighlight[num]
-  }
-
+  const toggleHighlight = (num) => {
+    fibonacciHighlight[num] = !fibonacciHighlight[num];
+  };
 </script>
 
 <div class="numberbar">
-  <div>Fibonnaci Numbers 
-    <button on:click={() => {
-    for (const val in fibonacciHighlight) {fibonacciHighlight[val] = false}
-  }}>clear</button>
-  <button on:click={() => {
-    for (const val in fibonacciHighlight) {fibonacciHighlight[val] = true}
-  }}>select all</button>
-   
+  <div>
+    Fibonnaci Numbers
+    <button
+      on:click={() => {
+        for (const val in fibonacciHighlight) {
+          fibonacciHighlight[val] = false;
+        }
+      }}>clear</button
+    >
+    <button
+      on:click={() => {
+        for (const val in fibonacciHighlight) {
+          fibonacciHighlight[val] = true;
+        }
+      }}>select all</button
+    >
   </div>
   <table>
     <thead>
       <tr>
         {#each $fibonacciNumbers as fibonacciNumber, i}
-          <td>{i+1}</td>
+          <td>{i + 1}</td>
         {/each}
-      
-
-</tr>
-  {#each $fibonacciNumbers as triangularNumber, i}
-  <td>
-    <a href="#" bind:innerHTML={triangularNumber} contenteditable 
-    class:fibonacciHighlight={fibonacciHighlight[triangularNumber]}
-    on:click|preventDefault={() => toggleHighlight(triangularNumber)}>
-      {triangularNumber} &nbsp;
-    </a>
-    </td>
-{/each}
-</thead>
-</table>
+      </tr>
+      {#each $fibonacciNumbers as triangularNumber, i}
+        <td>
+          <a
+            href="#"
+            bind:innerHTML={triangularNumber}
+            contenteditable
+            class:fibonacciHighlight={fibonacciHighlight[triangularNumber]}
+            on:click|preventDefault={() => toggleHighlight(triangularNumber)}
+          >
+            {triangularNumber} &nbsp;
+          </a>
+        </td>
+      {/each}
+    </thead>
+  </table>
 </div>
 
 <style>
@@ -61,21 +64,21 @@
     width: 100%;
     background-color: lightblue;
     border-top-left-radius: 1em;
-    border-top-right-radius: 1em;
-    }
+    /* border-top-right-radius: 1em; */
+  }
 
-    .fibonacciHighlight {
-		color: yellow;
-		background-color: red;
-		font-style: italic;
-		font-weight: bold;
-	}
+  .fibonacciHighlight {
+    color: yellow;
+    background-color: red;
+    font-style: italic;
+    font-weight: bold;
+  }
 
-  table, td {
+  table,
+  td {
     border-style: solid;
     text-align: center;
     border-collapse: collapse;
     padding: 0.5vh;
   }
-
 </style>
